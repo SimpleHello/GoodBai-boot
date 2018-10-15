75e3baf6ddd3d0cf0882f5342704f71e97242ed8
mag <mag@kongtrolink.com>
2018-10-15 11:28:43 +0800

    logging 日志配置 用到了 lombok

--- GoodBai-server/src/main/java/go/boot/server/controller/TestController.java
+++ GoodBai-server/src/main/java/go/boot/server/controller/TestController.java
@@ -1,18 +1,29 @@
 package go.boot.server.controller;
 
-import org.springframework.stereotype.Controller;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by Mg on 2018/4/8.
+ * RestController 用这个标签 直接返回Json
  */
-@Controller
+@Slf4j
+@RestController
 public class TestController {
 
     @RequestMapping("/helloTest")
-    public String hello() {
-        System.out.println("我进入了 hello");
-        return "helloTest";
+    public Map<String,Object> hello() {
+        Map<String,Object> map = new HashMap<>();
+        map.put("result","11");
+        map.put("message","helloTest");
+        log.info("我进入了 ---- info -----");
+        log.debug("我进入了---- debug -----");
+        log.error("我进入了---- error -----");
+        return map;
     }
 
 }
